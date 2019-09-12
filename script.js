827b531194d0b845634b9876d9020ddffbf96606
Jonathan Jones <jonathan.jones51@yahoo.com>
2019-09-12 11:58:39 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// <!-- TODO List -->
+
+// <!-- Check for win.-->
+// <!-- When checking for win check diagnol, side to side, and up and down. -->
+
+// <!-- When placing a chip check whether the spot below is taken by another chip.-->
+
+// <!-- 2d array -->
+
+// <!-- Alternate players -->
+
+// <!-- Array is a string of words. "black" "red" "empty" as a placeholder-->
+
+// <!-- function that decides what player starts -->
+
+// <!-- function for placing which column the chip will go and *click handler* functionputting the chip into the column -->
+
+// <!-- draw board and screen with css that sections -->
+
+// <!-- class for each color chip and class for chip. -->
+
+// <!-- create board in HTML -->
+
+// <!-- need to know dimensions of board -->
+
+// <!-- click handler -->
+
+// <!-- once 6 discs are added to a column do NOT allow anymore. -->
+
+// <!-- each column has divs inside of it. -->
+
+// <!-- columns made with flex -->
+
+// <!-- String of arrays that represents where chips are -->
+
+// <!-- fucntion that pushes a chip into a column -->
+
+// <!-- use css as only a way to display the board and chips.  -->
+
+// <!-- HTML will NEED seven columns with click handler(js) -->
+
+// let board = [
+//     [],
+//     [],
+//     [],
+//     [],
+//     [],
+//     []
+// ];
+
+function selectStartingPlayer() {
+
+}
+
+function checkForWin() {
+    checkForWinHorizontal()
+    checkForWinVertical()
+    checkForWinDiagonal()
+}
+
+function checkForWinHorizontal() {
+    for () {
+
+    }
+}
+
+function checkForWinVertical() {
+    for () {
+
+    }
+}
+
+function checkForWinDiagonal() {
+    for () {
+        
+    }
+}
+
